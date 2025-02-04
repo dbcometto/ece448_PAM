5d8aedebbf47634d8db3e6fa0982784a13533df2
dbcometto <111076949+dbcometto@users.noreply.github.com>
2025-02-04 14:54:34 -0700

    progress

--- PAM_main.py
+++ PAM_main.py
@@ -15,7 +15,8 @@ import os
 import sys
 sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
 
-from ascii2sym import ascii2sym  # grc-generated hier_block
+from ascii2float import ascii2float  # grc-generated hier_block
+from float2ascii import float2ascii  # grc-generated hier_block
 from gnuradio import blocks
 from gnuradio import gr
 from gnuradio.filter import firdes
@@ -120,25 +121,42 @@ class PAM_main(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.blocks_vector_source_x_0 = blocks.vector_source_b((90,111), True, 1, [msg_start])
-        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
-        self.ascii2sym_1 = ascii2sym(
+        self.float2ascii_0 = float2ascii(
             bits_per_sym=1,
+            gain=1,
             invert_bits=0,
-            is_7_bit_ascii=1,
-            is_LSB=1,
+            is_7_bit_ascii=0,
+            is_LSB=0,
             is_polar=1,
+            samp_rate=32000,
+            symbol_delay=0,
         )
 
-        self.top_layout.addWidget(self.ascii2sym_1)
+        self.top_layout.addWidget(self.float2ascii_0)
+        self.blocks_vector_source_x_0 = blocks.vector_source_b((90,111), True, 1, [msg_start])
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, 'C:\\ece448_PAM\\test_output.bin', False)
+        self.blocks_file_sink_0.set_unbuffered(False)
+        self.ascii2float_0 = ascii2float(
+            bits_per_sym=1,
+            invert_bits=0,
+            is_7_bit_ascii=0,
+            is_LSB=0,
+            is_polar=1,
+            samp_rate=32000,
+        )
+
+        self.top_layout.addWidget(self.ascii2float_0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.ascii2sym_1, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.ascii2float_0, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.float2ascii_0, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.blocks_vector_source_x_0, 0), (self.ascii2sym_1, 0))
+        self.connect((self.blocks_vector_source_x_0, 0), (self.ascii2float_0, 0))
+        self.connect((self.float2ascii_0, 0), (self.blocks_file_sink_0, 0))
 
 
     def closeEvent(self, event):
