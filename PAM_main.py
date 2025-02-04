e39dd62d7aa3f5e2be97f2f0c76b9bf07e2cc940
dbcometto <111076949+dbcometto@users.noreply.github.com>
2025-02-04 14:36:02 -0700

    Ascii 2 Sym is working well!

--- PAM_main.py
+++ PAM_main.py
@@ -25,6 +25,7 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+import pmt
 import sip
 
 
@@ -65,13 +66,14 @@ class PAM_main(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 32000
+        self.msg_start = msg_start = gr.tag_utils.python_to_tag((0, pmt.intern("Start"), pmt.intern("Start"), pmt.intern("src")))
 
         ##################################################
         # Blocks
         ##################################################
 
         self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-            1024, #size
+            20, #size
             samp_rate, #samp_rate
             "", #name
             1, #number of inputs
@@ -83,11 +85,11 @@ class PAM_main(gr.top_block, Qt.QWidget):
         self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
 
         self.qtgui_time_sink_x_0.enable_tags(True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_TAG, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "Start")
+        self.qtgui_time_sink_x_0.enable_autoscale(True)
         self.qtgui_time_sink_x_0.enable_grid(False)
         self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_control_panel(True)
         self.qtgui_time_sink_x_0.enable_stem_plot(False)
 
 
@@ -118,25 +120,25 @@ class PAM_main(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.blocks_vector_source_x_0 = blocks.vector_source_b((90, 111), True, 1, [])
+        self.blocks_vector_source_x_0 = blocks.vector_source_b((90,111), True, 1, [msg_start])
         self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
-        self.ascii2sym_0 = ascii2sym(
-            M=0,
+        self.ascii2sym_1 = ascii2sym(
             bits_per_sym=1,
             invert_bits=0,
-            is_7_bit_ascii=0,
-            pol=0,
+            is_7_bit_ascii=1,
+            is_LSB=1,
+            is_polar=1,
         )
 
-        self.top_layout.addWidget(self.ascii2sym_0)
+        self.top_layout.addWidget(self.ascii2sym_1)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.ascii2sym_0, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.ascii2sym_1, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.blocks_vector_source_x_0, 0), (self.ascii2sym_0, 0))
+        self.connect((self.blocks_vector_source_x_0, 0), (self.ascii2sym_1, 0))
 
 
     def closeEvent(self, event):
@@ -154,6 +156,13 @@ class PAM_main(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate
         self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+
+    def get_msg_start(self):
+        return self.msg_start
+
+    def set_msg_start(self, msg_start):
+        self.msg_start = msg_start
+        self.blocks_vector_source_x_0.set_data((90,111), [self.msg_start])
 
 
 
